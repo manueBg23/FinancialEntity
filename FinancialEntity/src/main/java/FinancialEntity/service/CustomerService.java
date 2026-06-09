a3c5e589d61342ba3b3a03eff6cbc8b1956a43cc
Manuel <begambresuarezm@gmail.com>
2026-06-09 11:39:29 -0500

    fix: add new logical

--- FinancialEntity/src/main/java/FinancialEntity/service/CustomerService.java
+++ FinancialEntity/src/main/java/FinancialEntity/service/CustomerService.java
@@ -10,20 +10,37 @@ public class CustomerService implements CustomerRepository{
 
     private CustomerRepository customerRepository = new CustomerService();
 
+    public String findById(String id){
+        /*if (id <= 18){
+
+        }*/
+
+        //menor que 18, imprimir= menor de edad el usuario no se puede crear
+        //mayor que 18, enviar el id a createCustomer
+        //es necesario tener en cuenta la fehca de nacimiento porque aqui se valida
+
+        return id;
+    }
+
     public Customer createCustomer(Customer customer){
 
         customer.setId(UUID.randomUUID().toString());
         customer.setCreateDate(LocalDate.now());
         customerRepository.upDateCustomer(customer);
 
+        //aqui se calculara la fecha de creacion
+
         return customer;
     }
 
     public Customer upDateCustomer(Customer customer){
+
+        //se modificara y se guardara con fecha
         return customer;
     }
 
     public Customer deleteCustomer(Customer customer){
+        //se valida si tiene productos, por ahora dejar
         return customer;
     }
 
